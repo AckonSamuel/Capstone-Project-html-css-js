61d9e71f62047e59b37f02fc18712a63c928e8ef
Samuel Ackon <ackons433@gmail.com>
2022-02-07 18:00:58 +0000

    setting up trainers dynamically

---
+++ trainers.js
@@ -0,0 +1,64 @@
+/* eslint linebreak-style: ["error", "windows"] */
+const trainers = [
+  {
+    img: 'assets/images/Micheal.png',
+    name: 'Micheal Angelou',
+    background: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque soluta est laudantium totam earum! Possimus, odio necessitatibus quibusdam repellat dignissimos, asperiores temporibus esse',
+  },
+  {
+    img: 'assets/images/Micheal.png',
+    name: 'Micheal Angelou',
+    background: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque soluta est laudantium totam earum! Possimus, odio necessitatibus quibusdam repellat dignissimos, asperiores temporibus esse',
+  },
+  {
+    img: 'assets/images/Micheal.png',
+    name: 'Micheal Angelou',
+    background: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque soluta est laudantium totam earum! Possimus, odio necessitatibus quibusdam repellat dignissimos, asperiores temporibus esse',
+  },
+  {
+    img: 'assets/images/Micheal.png',
+    name: 'Micheal Angelou',
+    background: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque soluta est laudantium totam earum! Possimus, odio necessitatibus quibusdam repellat dignissimos, asperiores temporibus esse',
+  },
+  {
+    img: 'assets/images/Micheal.png',
+    name: 'Micheal Angelou',
+    background: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque soluta est laudantium totam earum! Possimus, odio necessitatibus quibusdam repellat dignissimos, asperiores temporibus esse',
+  },
+  {
+    img: 'assets/images/Micheal.png',
+    name: 'Micheal Angelou',
+    background: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque soluta est laudantium totam earum! Possimus, odio necessitatibus quibusdam repellat dignissimos, asperiores temporibus esse',
+  },
+];
+
+const getTrainers = document.querySelector('trainers');
+const getGridTrainers = document.createElement('div');
+getGridTrainers.classList.add('grid-trainers-mobile');
+getTrainers.appendChild(getGridTrainers);
+const chess = document.createElement('div');
+chess.classList.add('chess');
+
+const trainer1 = document.createElement('div');
+trainer1.classList.add('trainer1');
+
+const trainer2 = document.createElement('div');
+trainer2.classList.add('trainer2');
+
+const trainer3 = document.createElement('div');
+trainer3.classList.add('trainer3');
+
+const trainer4 = document.createElement('div');
+trainer4.classList.add('trainer4');
+
+const trainer5 = document.createElement('div');
+trainer5.classList.add('trainer5');
+
+const trainer6 = document.createElement('div');
+trainer6.classList.add('trainer6');
+
+const chess = document.createElement('div');
+chess.classList.add('chess');
+chess.
+const info = document.createElement('div');
+info.classList.add('info');
